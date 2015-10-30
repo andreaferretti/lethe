020e372532a2c70559a29de478cc791b79cf92b3
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-10-30 17:25:13 +0100

    Removed println

--- shared/src/main/scala/unicredit/oram/TrivialORAM.scala
+++ shared/src/main/scala/unicredit/oram/TrivialORAM.scala
@@ -5,18 +5,14 @@ import scala.concurrent.{ Future, ExecutionContext }
 
 trait TrivialORAM[Id, Doc] extends ORAM[Id, Doc] with Client[Id, Doc] {
   override def readAndRemove(id: Id) = {
-    println("id: ", id)
 
     remote.capacity flatMap { n =>
-      println("capacity: ", n)
-
       (0 until n).foldLeft(Future(None: Option[Doc])) { (f, i) =>
         for {
           (uid, doc) <- fetchClear(i)
           next <- if (id == uid) Future(Some(doc)) else f
           data = if (id == uid) (uid, empty) else (uid, doc)
           _ <- putClear(i, data)
-          _ = println("at element", i)
         } yield next
       }
     }
--- shared/src/main/scala/unicredit/oram/UnencryptedClient.scala
+++ shared/src/main/scala/unicredit/oram/UnencryptedClient.scala
@@ -9,18 +9,13 @@ trait UnencryptedClient extends Client[Int, String] {
   def decrypt(a: Array[Byte]) = {
     val n = BigInt(a take 4).toInt
 
-    println("Decrypting ", (a take 4).toList, " which amounts to ", n)
-
     (n, new String(a drop 4, "UTF-8"))
   }
 
   def encrypt(data: (Int, String)) = {
-    println("About to encrypt ", data)
     val (n, doc) = data
     val head = BigInt(n).toByteArray
     val padding = Array.fill[Byte](4 - head.length)(0)
-
-    println("key is ", (padding ++ head).toList, " which amounts to ", n)
 
     padding ++ head ++ doc.getBytes("UTF-8")
   }
