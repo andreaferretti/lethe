14b79f7a6b10a639d645d83d56992eaf2a88f0a0
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-11-06 10:38:47 +0100

    Minor

--- server/src/main/scala/unicredit/oram/server/ZMQServer.scala
+++ server/src/main/scala/unicredit/oram/server/ZMQServer.scala
@@ -1,15 +1,19 @@
 package unicredit.oram
 package server
 
+import scala.util.Try
+
 import org.zeromq.ZMQ
 
 
 object ZMQServer extends App {
-  val url = "tcp://*:8888"
+  val port = Try(args(0).toInt) getOrElse 8888
+  val url = s"tcp://*:$port"
   val context = ZMQ.context(1)
   val socket = context.socket(ZMQ.REP)
 
   socket.bind(url)
+  println(s"Server ready on port $port")
 
   val backend = new MemoryBackend
 
