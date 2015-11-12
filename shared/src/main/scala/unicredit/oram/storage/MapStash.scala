6c0a664daf36e056da07b4a9cf69c0003beff2dd
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-11-12 19:04:52 +0100

    Stash serialization

--- shared/src/main/scala/unicredit/oram/storage/MapStash.scala
+++ shared/src/main/scala/unicredit/oram/storage/MapStash.scala
@@ -1,9 +1,13 @@
 package unicredit.oram
 package storage
 
+import boopickle.Default._
 
-class MapStash[Id, Doc] extends Stash[Id, Doc] {
-  var stash = Map.empty[Id, Doc]
+import serialization.BooSerializer
+
+
+class MapStash[Id: Pickler, Doc: Pickler](s: Map[Id, Doc]) extends Stash[Id, Doc] {
+  var stash = s
 
   override def getOrElse(id: Id, default: Doc) = stash.getOrElse(id, default)
   override def ++=(xs: Seq[(Id, Doc)]) = stash ++= xs
@@ -13,8 +17,18 @@ class MapStash[Id, Doc] extends Stash[Id, Doc] {
   }
   override def take(n: Int)(f: (Id, Doc) => Boolean) =
     stash filter f.tupled take n
+
+  override def serialize = {
+    val s = new BooSerializer[Map[Id, Doc]]
+    s.encode(stash)
+  }
 }
 
 object MapStash {
-  def empty[Id, Doc] = new MapStash[Id, Doc]
+  def empty[Id: Pickler, Doc: Pickler] = new MapStash[Id, Doc](Map())
+
+  def apply[Id: Pickler, Doc: Pickler](a: Array[Byte]) = {
+    val s = new BooSerializer[Map[Id, Doc]]
+    new MapStash(s.decode(a))
+  }
 }
--- shared/src/main/scala/unicredit/oram/storage/Stash.scala
+++ shared/src/main/scala/unicredit/oram/storage/Stash.scala
@@ -8,4 +8,6 @@ trait Stash[Id, Doc] {
   def --=(xs: Set[Id]): Unit
   def filter(f: (Id, Doc) => Boolean): Unit
   def take(n: Int)(f: (Id, Doc) => Boolean): Map[Id, Doc]
+
+  def serialize: Array[Byte]
 }
