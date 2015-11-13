2fbfb101448da80f79641466201de0000dc10be9
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-11-13 16:19:49 +0100

    Minor

--- shared/src/main/scala/unicredit/oram/sync/MultiORAM.scala
+++ shared/src/main/scala/unicredit/oram/sync/MultiORAM.scala
@@ -8,21 +8,6 @@ import client._
 import storage._
 
 
-case class Bijection[A, B](from: A => B, to: B => A)
-
-class WrapORAM[K, V, K1 <: K, V1 <:V, Id, Doc] (
-  inner: PathORAM[K, V, K1, V1],
-  bijId: Bijection[Id, K1],
-  bijDoc: Bijection[Doc, V1]
-) extends ORAM[Id, Doc] {
-  override def read(id: Id) = bijDoc.to(inner.read(bijId.from(id)))
-
-  override def write(id: Id, doc: Doc) =
-    inner.write(bijId.from(id), bijDoc.from(doc))
-
-  override def init = inner.init
-}
-
 object MultiORAM {
   import boopickle.Default._
   import java.security.SecureRandom
---
+++ shared/src/main/scala/unicredit/oram/sync/WrapORAM.scala
@@ -0,0 +1,18 @@
+package unicredit.oram
+package sync
+
+
+case class Bijection[A, B](from: A => B, to: B => A)
+
+class WrapORAM[K, V, K1 <: K, V1 <:V, Id, Doc] (
+  inner: PathORAM[K, V, K1, V1],
+  bijId: Bijection[Id, K1],
+  bijDoc: Bijection[Doc, V1]
+) extends ORAM[Id, Doc] {
+  override def read(id: Id) = bijDoc.to(inner.read(bijId.from(id)))
+
+  override def write(id: Id, doc: Doc) =
+    inner.write(bijId.from(id), bijDoc.from(doc))
+
+  override def init = inner.init
+}
