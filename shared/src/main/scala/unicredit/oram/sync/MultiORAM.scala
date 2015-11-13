76c1bb49800e52fd7eb1ae5fb53dfbb716dd70c8
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-11-13 15:34:15 +0100

    Simplified MultiORAM

--- shared/src/main/scala/unicredit/oram/sync/MultiORAM.scala
+++ shared/src/main/scala/unicredit/oram/sync/MultiORAM.scala
@@ -8,51 +8,51 @@ import client._
 import storage._
 
 
-class LeftMultiORAM[Id, Doc, Id1, Doc1] (
-  inner: PathORAM[Either[Id, Id1], Either[Doc, Doc1], Left[Id, Id1], Left[Doc, Doc1]]
-) extends ORAM[Id, Doc] {
-  override def read(id: Id) = inner.read(Left(id)).left.get
+case class Bijection[A, B](from: A => B, to: B => A)
 
-  override def write(id: Id, doc: Doc) = inner.write(Left(id), Left(doc))
+class WrapORAM[K, V, K1 <: K, V1 <:V, Id, Doc] (
+  inner: PathORAM[K, V, K1, V1],
+  bijId: Bijection[Id, K1],
+  bijDoc: Bijection[Doc, V1]
+) extends ORAM[Id, Doc] {
+  override def read(id: Id) = bijDoc.to(inner.read(bijId.from(id)))
+
+  override def write(id: Id, doc: Doc) =
+    inner.write(bijId.from(id), bijDoc.from(doc))
 
   override def init = inner.init
 }
-
-class RightMultiORAM[Id, Doc, Id1, Doc1] (
-  inner: PathORAM[Either[Id, Id1], Either[Doc, Doc1], Right[Id, Id1], Right[Doc, Doc1]]
-) extends ORAM[Id1, Doc1] {
-  override def read(id: Id1) = inner.read(Right(id)).right.get
-
-  override def write(id: Id1, doc: Doc1) = inner.write(Right(id), Right(doc))
-
-  override def init = ()
-}
-
 
 object MultiORAM {
   import boopickle.Default._
   import java.security.SecureRandom
 
+  def leftBijection[A, B] = Bijection[A, Left[A, B]](Left(_), _.left.get)
+  def rightBijection[A, B] = Bijection[B, Right[A, B]](Right(_), _.right.get)
+
   def pair[
-    Id: Pointed: Pickler,
-    Doc: Pointed: Pickler,
     Id1: Pointed: Pickler,
-    Doc1: Pointed: Pickler
+    Doc1: Pointed: Pickler,
+    Id2: Pointed: Pickler,
+    Doc2: Pointed: Pickler
   ](remote: Remote, passPhrase: String, params: Params) = {
-    type K = Either[Id, Id1]
-    type V = Either[Doc, Doc1]
-    type K1 = Left[Id, Id1]
-    type V1 = Left[Doc, Doc1]
-    type K2 = Right[Id, Id1]
-    type V2 = Right[Doc, Doc1]
-    implicit val p1 = implicitly[Pointed[Id]].map(Left[Id, Id1](_))
-    implicit val p2 = implicitly[Pointed[Id1]].map(Right[Id, Id1](_))
-    implicit val p3 = implicitly[Pointed[Doc]].map(Left[Doc, Doc1](_))
-    implicit val p4 = implicitly[Pointed[Doc1]].map(Right[Doc, Doc1](_))
+    type K = Either[Id1, Id2]
+    type V = Either[Doc1, Doc2]
+    type K1 = Left[Id1, Id2]
+    type V1 = Left[Doc1, Doc2]
+    type K2 = Right[Id1, Id2]
+    type V2 = Right[Doc1, Doc2]
+    implicit val p1 = implicitly[Pointed[Id1]].map(Left[Id1, Id2](_))
+    implicit val p2 = implicitly[Pointed[Id2]].map(Right[Id1, Id2](_))
+    implicit val p3 = implicitly[Pointed[Doc1]].map(Left[Doc1, Doc2](_))
+    implicit val p4 = implicitly[Pointed[Doc2]].map(Right[Doc1, Doc2](_))
     val (left, right) =
       make2[K, V, K1, V1, K2, V2](remote, passPhrase, params)
 
-    (new LeftMultiORAM(left), new RightMultiORAM(right))
+    (
+      new WrapORAM(left, leftBijection[Id1, Id2], leftBijection[Doc1, Doc2]),
+      new WrapORAM(right, rightBijection[Id1, Id2], rightBijection[Doc1, Doc2])
+    )
   }
 
   def make2[
