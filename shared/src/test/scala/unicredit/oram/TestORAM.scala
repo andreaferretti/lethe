bae8167fb79b2dd450278dfd64d2c8728c001d4f
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-11-14 12:49:12 +0100

    Factored test

--- shared/src/test/scala/unicredit/oram/TestORAM.scala
+++ shared/src/test/scala/unicredit/oram/TestORAM.scala
@@ -8,19 +8,20 @@ import transport.MemoryRemote
 
 
 object ORAMSpec extends SimpleTestSuite {
+  def flip[A, B](x: (A, B)) = (x._2, x._1)
+
+  val elements = List(
+      "this is a secret",
+      "this is secret as well",
+      "strictly confidential",
+      "cippa lippa"
+    ).zipWithIndex map flip
+
+  implicit val pint = Pointed(-1)
+  implicit val pstring = Pointed("")
+  val params = Params(depth = 8, bucketSize = 4)
 
   test("The local path ORAM should be able to write and retrieve keys") {
-    def flip[A, B](x: (A, B)) = (x._2, x._1)
-
-    val elements = (List.fill(50)(List(
-        "this is a secret",
-        "this is secret as well",
-        "strictly confidential",
-        "cippa lippa"
-      ))).flatten.zipWithIndex map flip
-    implicit val pint = Pointed(-1)
-    implicit val pstring = Pointed("")
-    val params = Params(depth = 8, bucketSize = 4)
     val remote = new MemoryRemote(capacity = params.numSlots)
     val oram = PathORAM[Int, String, Int, String](remote, "Hello world", params)
 
@@ -30,11 +31,29 @@ object ORAMSpec extends SimpleTestSuite {
     }
     assertEquals(oram.read(2), "strictly confidential")
     assertEquals(oram.read(0), "this is a secret")
+  }
 
+  test("A missing key should result in an empty doc") {
+    val remote = new MemoryRemote(capacity = params.numSlots)
+    val oram = PathORAM[Int, String, Int, String](remote, "Hello world", params)
+
+    oram.init
+    for ((id, doc) <- elements) {
+      oram.write(id, doc)
+    }
+    assertEquals(oram.read(12), "")
+  }
+
+  test("The local path ORAM should be able to overwrite keys") {
+    val remote = new MemoryRemote(capacity = params.numSlots)
+    val oram = PathORAM[Int, String, Int, String](remote, "Hello world", params)
+
+    oram.init
+    for ((id, doc) <- elements) {
+      oram.write(id, doc)
+    }
+    assertEquals(oram.read(2), "strictly confidential")
     oram.write(2, "new secret")
-
     assertEquals(oram.read(2), "new secret")
-    assertEquals(oram.read(125), "this is secret as well")
-    assertEquals(oram.read(525), "")
   }
 }
