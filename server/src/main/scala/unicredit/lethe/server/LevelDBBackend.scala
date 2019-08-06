8812e7eaa6c2b18c230be54cd4648d6128982cdc
Marco F <mfirry@users.noreply.github.com>
2019-08-06 09:24:05 +0200

    minor

--- server/src/main/scala/unicredit/lethe/server/LevelDBBackend.scala
+++ server/src/main/scala/unicredit/lethe/server/LevelDBBackend.scala
@@ -1,26 +1,25 @@
 /* Copyright 2016 UniCredit S.p.A.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package unicredit.lethe
 package server
 
 import java.io.File
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 import org.iq80.leveldb._
 import org.iq80.leveldb.impl.Iq80DBFactory._
-
 
 class LevelDBBackend(file: File) extends Backend {
   private val options = new Options()
@@ -30,7 +29,7 @@ class LevelDBBackend(file: File) extends Backend {
 
   override def capacity = {
     if (capacityCache == 0) {
-      capacityCache = db.iterator.size
+      capacityCache = db.iterator.asScala.size
     }
     capacityCache
   }
--- server/src/main/scala/unicredit/lethe/server/ZMQServer.scala
+++ server/src/main/scala/unicredit/lethe/server/ZMQServer.scala
@@ -1,17 +1,17 @@
 /* Copyright 2016 UniCredit S.p.A.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package unicredit.lethe
 package server
 
@@ -19,7 +19,6 @@ import scala.util.Try
 import java.io.File
 
 import org.zeromq.ZMQ
-
 
 object ZMQServer extends App {
   val port = Try(args(0).toInt) getOrElse 8888
@@ -36,7 +35,7 @@ object ZMQServer extends App {
   while (true) {
     Message.fromBytes(socket.recv) match {
       case Capacity() => socket.send(Bytes(backend.capacity))
-      case Fetch(n) => socket.send(backend.fetch(n))
+      case Fetch(n)   => socket.send(backend.fetch(n))
       case Put(n, doc) =>
         backend.put(n, doc)
         socket.send("ok")
